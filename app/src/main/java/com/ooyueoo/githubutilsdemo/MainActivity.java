f47c29739ff5c2ffdb084e5c5228c010005c845d
ooyueoo <oozrzyoo@sina.com>
2019-05-23 00:04:37 +0800

    提交

---
+++ app/src/main/java/com/ooyueoo/githubutilsdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ooyueoo.githubutilsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mylibrary/src/main/java/com/ooyueoo/mylibrary/GithubUtilsDemoTest.java
@@ -0,0 +1,10 @@
+package com.ooyueoo.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class GithubUtilsDemoTest {
+    public  void sayHellow(Context context){
+        Toast.makeText(context, "测试", Toast.LENGTH_SHORT).show();
+    }
+}
